8b6b03fcca13b06af5ef9eb7020cc81ec5ac58c1
Eren Kaya <95748211+ErenKaya31@users.noreply.github.com>
2024-01-09 22:03:11 +0300

    Update ClientPrefs.hx

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -74,6 +74,7 @@ import states.TitleState;
 	public var badWindow:Int = 135;
 	public var safeFrames:Float = 10;
 	public var guitarHeroSustains:Bool = true;
+	public var naughtie:Bool = false;
 	public var discordRPC:Bool = true;
 }
 
